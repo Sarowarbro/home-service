7f1c4901a630279928536da03993aa7d9aa492f4
Sarowar Chowdhury <74640894+Sarowarbro@users.noreply.github.com>
2022-11-13 10:45:35 +0600

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,415 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>The home service market place</title>
+    <!-- google-fonts -->
+    <link href="//fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+    <!-- //google-fonts -->
+    <!-- Template CSS Style link -->
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+</head>
+
+<body>
+    <!--header-->
+    <header id="site-header" class="fixed-top">
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg stroke">
+                <!-- <h1>
+                    <a class="navbar-brand d-flex align-items-center" href="index.html">
+                        <i class="fa fa-yelp mr-1" aria-hidden="true"></i>Fitment</a>
+                </h1> -->
+               
+    <a class="navbar-brand" href="#index.html">
+        <img src="assets/images/Logo.svg" alt="Your logo" title="Your logo" style="height:100px;" />
+    </a>
+                <button class="navbar-toggler  collapsed bg-gradient" type="button" data-toggle="collapse"
+                    data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+                    <span class="navbar-toggler-icon fa icon-close fa-times"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav ml-lg-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link"  href="#Who_We_Are">Who We Are </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#What_We_Do">What We Do</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link"  href="#Our_Services">Our Services </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link"  href="#Contact_Us">Contact Us</a>
+                        </li>
+                       
+                    </ul>
+                </div>
+                <!-- toggle switch for light and dark theme -->
+                <div class="cont-ser-position">
+                    <nav class="navigation">
+                        <div class="theme-switch-wrapper">
+                            <label class="theme-switch" for="checkbox">
+                                <input type="checkbox" id="checkbox">
+                                <div class="mode-container">
+                                    <i class="gg-sun"></i>
+                                    <i class="gg-moon"></i>
+                                </div>
+                            </label>
+                        </div>
+                    </nav>
+                </div>
+                <!-- //toggle switch for light and dark theme -->
+            </nav>
+        </div>
+    </header>
+    <!--//header-->
+
+    <!-- banner section -->
+    <section id="home" class="main-banner py-5">
+        <div class="container pt-5 pb-md-4">
+            <div class="row align-items-center">
+                <div class="col-md-6 pt-md-0 pt-4">
+                    <h3 class="mb-sm-4 mb-3 title">  
+                        <span>Quality Clicks, Delivered</span></h3>
+                    <p>The HomeService Marketplace is just that. A platform for all home service brands to buy and sell traffic, simplified. 
+                        We help businesses scale—locally, regionally or nationally—regardless of the size of your organization or footprint. </p>
+                    <div class="mt-md-5 mt-4 mb-lg-0 mb-4">
+                        <a class="btn btn-style" href="#Contact_Us">Let's Talk</a>
+                    </div>
+                </div>
+                <div class="col-md-6 banner-right mt-md-0 mt-4 text-right">
+                    <img class="img-fluid" src="assets/images/banner2.jpg" alt=" ">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //banner section -->
+
+    <!-- banner bottom section -->
+    <section class="main-aboutblock py-5" id="Who_We_Are">
+        <div class="container py-md-5 py-sm-4">
+            <div class="row">
+                <div class="col-lg-5 left-wthree-img mb-lg-0 mb-5">
+                    <img class="img-fluid img-responsive" src="assets/images/about.png" alt=" ">
+                </div>
+                <div class="col-lg-7 about-right-faq align-self pl-lg-5">
+                    <h3 class="title-style mb-2">Who We Are</h3>
+                    <p class="mt-3">As Lead Generation and Home Services specialists, our agency model strives to simplify digital marketing acquisition in one place. </p>
+                    <p class="mt-3">We recognized a need in a growing vertical to centralize lead generation and site monetization into one platform. Our goal is help Advertisers drive high intent, targeted consumers to their brands, while leveraging the most scalable and cost effective technologies available.</p>
+               
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //banner bottom section -->
+
+   <!-- banner bottom section -->
+   <section class="main-aboutblock py-5" id="What_We_Do">
+    <div class="container py-md-5 py-sm-4">
+        <div class="row">
+           
+            <div class="col-lg-7 about-right-faq align-self pl-lg-5">
+                <h3 class="title-style mb-2">What we Do</h3>
+                <p class="mt-3">Bridge the gap between lead buyers and sellers in Home Services. We aggregate supply and demand of targeted click traffic, and past the cost savings of our efficiency to advertisers and publishers </p>
+                <p class="mt-3">You select the region, service type and criteria. We’ll serve your branded ad, on targeted websites, based on your selection. You can start, cap, pause and resume click traffic at any time.</p>
+           
+            </div>
+            <div class="col-lg-5 left-wthree-img mb-lg-0 mb-5">
+                <img class="img-fluid img-responsive" src="assets/images/about1.png" alt=" ">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- //banner bottom section -->
+    <!-- grids section -->
+    <section class="main-grids-sec py-5" id="Our_Services">
+        <div class="container py-md-4">
+            <div class="row justify-content-center text-center">
+                <div class="col-md-8">
+                    <div class="section-heading mb-sm-5 mb-4">
+                        <h3 class="title-style mb-2">Our services</h3>
+                        <h5 class="font-weight-bold mb-2">For Brands</h5>
+                        <p class="lead">
+                           We offer both, self-serve or Full-service models, both equal access to highly targeted homeowner traffic with specific filter criteria options, to customize consumers being driven to your brand landing page.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row text-center justify-content-center">
+                <div class="col-lg-4 col-md-6 col-sm-6 back-image-sec p-0">
+                    <div class="grid-content-sec column-content p-4">
+
+                        <p class="para">Access to a growing number of private media placements targeting consumers in various home services sub verticals. </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 p-0">
+                    <div class="img-column">
+                        <img src="assets/images/grid2.jpg" alt="product" class="img-responsive rounded-0 ">
+                    </div>
+                    <div class="column-content mt-sm-4 p-4">
+                     
+                        <p class="para">We believe success should be based on the ROI for your marketing investment. That’s why performance marketing matters</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 p-0 mt-lg-0 mt-4 d-grid-res">
+                    <div class="column-content respon-order-2 mb-lg-4 p-4">
+                     
+                        <p class="para">Don’t waste your marketing budget on weak leads that rarely close. Instead, invest in scalable, pay-for-performance options customized specifically for your business goals.</p>
+                    </div>
+                    <div class="img-column respon-order-1">
+                        <img src="assets/images/grid3.jpg" alt="product" class="img-responsive rounded-0">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //grids section -->
+
+    <!-- image with content section -->
+    <section class="image-with-content py-sm-5 py-4">
+        <div class="container py-md-5">
+            
+            <div class="row align-items-center">
+                <div class="col-lg-5 col-md-5 content-sec-1">
+                    <h4><a href="services.html" class="title-style-anchor">For Publishers</a></h4>
+                    <p>Our goal is to help you maximize revenue from each website visitor. With a single step integration, we provide a value add to your consumers, while monetizing your confirmation page impressions all in one place. This allows you to “set and forget” a solution that will add profit to the bottom line and upside to your business. 
+                    </p>
+                   
+                </div>
+                <div class="col-lg-7 col-md-7 pl-lg-5 mt-md-0 mt-sm-5 mt-4">
+                    <img src="assets/images/image1.jpg" alt="product" class="img-responsive">
+                </div>
+            </div>
+          
+        </div>
+    </section>
+    <!-- //image with content section -->
+
+ 
+ <!-- contact -->
+ <section class="main-contact pb-5" id="Contact_Us">
+    <div class="container pb-md-5 pb-4">
+        <div class="row contact-block">
+            <div class="col-lg-7 contact-right mt-0">
+                <h3 class="title-style mb-2 pb-2">Contact Us</h3>
+                <p class="pb-2">Ready to take your business to the next level? So are we</p>
+                <form action="https://sendmail.homelayouts.com/submitForm" method="post" class="signin-form">
+                    <div class="input-grids">
+                        <input type="text" name="Name" id="Name" placeholder="Full Name*"
+                            class="contact-input" required="" />
+                        <input type="email" name="Email" id="Email" placeholder="Your Email*"
+                            class="contact-input" required="" />
+                        <input type="text" name="Phone" id="Phone" placeholder="Phone*"
+                            class="contact-input" required="" />
+                             <input type="text" name="Company" id="Company" placeholder="Company Name/Website*"
+                            class="contact-input" required="" />
+                    </div>
+                    <div class="form-input">
+                        <textarea name="message" id="message" placeholder="Type your message here*"
+                            required=""></textarea>
+                    </div>
+                    <button class="btn btn-style mt-sm-3">Let’s Talk</button>
+                </form>
+
+               
+            </div>
+            <div class="col-lg-5 mt-lg-0 mt-5 pl-xl-5 pl-lg-4">
+                <div class="contact-left mt-5 pt-lg-4">
+                    <div class="row cont-details">
+                        <div class="col-sm-12">
+                            <div class="d-flex contact-grid">
+                                <div class="cont-left text-center mr-3">
+                                    <span class="fa fa-globe"></span>
+                                </div>
+                                <div class="cont-right">
+                                    <h6>Company Address</h6>
+                                    <p>3370 N Hayden RD # 123 Scottsdale, AZ 85251</p>
+                                </div>
+                            </div>
+                            <div class="d-flex contact-grid mt-4 pt-lg-2">
+                                <div class="cont-left text-center mr-3">
+                                    <span class="fa fa-phone"></span>
+                                </div>
+                                <div class="cont-right">
+                                    <h6>Call Us</h6>
+                                    <p><a href="tel:818-519-6513">818-519-6513</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 mt-4">
+                            <div class="d-flex contact-grid">
+                                <div class="cont-left text-center mr-3">
+                                    <span class="fa fa-envelope-open"></span>
+                                </div>
+                                <div class="cont-right">
+                                    <h6>Email Us</h6>
+                                    <p><a href="mailto:info@montecitodigital.com" class="mail">info@montecitodigital.com</a></p>
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- //contact -->
+  
+    <!-- copyright -->
+    <section class="main-copyright">
+        <div class="container">
+            <div class="row bottom-copies">
+                <p class="col-lg-8 copy-footer-29">© 2022 Montecito Digital LLC
+                       </p>
+                <div class="col-lg-4 text-right">
+                    <div class="main-social-footer-29">
+                        <a href="#facebook" class="facebook"><span class="fa fa-facebook"></span></a>
+                        <a href="#twitter" class="twitter"><span class="fa fa-twitter"></span></a>
+                        <a href="#google"><span class="fa fa-google-plus" aria-hidden="true"></span></a>
+                        <a href="#instagram" class="instagram"><span class="fa fa-instagram"></span></a>
+                        <a href="#linkedin" class="linkedin"><span class="fa fa-linkedin"></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //copyright -->
+
+    <!-- Js scripts -->
+    <!-- move top -->
+    <button onclick="topFunction()" id="movetop" title="Go to top">
+        <span class="fa fa-level-up" aria-hidden="true"></span>
+    </button>
+    <script>
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("movetop").style.display = "block";
+            } else {
+                document.getElementById("movetop").style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    </script>
+    <!-- //move top -->
+
+    <!-- common jquery plugin -->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <!-- //common jquery plugin -->
+
+    <!-- gallery popup js -->
+    <script src="assets/js/smartphoto.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const sm = new SmartPhoto(".js-img-viwer", {
+                showAnimation: false
+            });
+            // sm.destroy();
+        });
+    </script>
+    <!-- //gallery popup js -->
+
+    <!-- theme switch js (light and dark)-->
+    <script src="assets/js/theme-change.js"></script>
+    <script>
+        function autoType(elementClass, typingSpeed) {
+            var thhis = $(elementClass);
+            thhis.css({
+                "position": "relative",
+                "display": "inline-block"
+            });
+            thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+            thhis = thhis.find(".text-js");
+            var text = thhis.text().trim().split('');
+            var amntOfChars = text.length;
+            var newString = "";
+            thhis.text("|");
+            setTimeout(function () {
+                thhis.css("opacity", 1);
+                thhis.prev().removeAttr("style");
+                thhis.text("");
+                for (var i = 0; i < amntOfChars; i++) {
+                    (function (i, char) {
+                        setTimeout(function () {
+                            newString += char;
+                            thhis.text(newString);
+                        }, i * typingSpeed);
+                    })(i + 1, text[i]);
+                }
+            }, 1500);
+        }
+
+        $(document).ready(function () {
+            // Now to start autoTyping just call the autoType function with the 
+            // class of outer div
+            // The second paramter is the speed between each letter is typed.   
+            autoType(".type-js", 200);
+        });
+    </script>
+    <!-- //theme switch js (light and dark)-->
+
+    <!-- MENU-JS -->
+    <script>
+        $(window).on("scroll", function () {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 80) {
+                $("#site-header").addClass("nav-fixed");
+            } else {
+                $("#site-header").removeClass("nav-fixed");
+            }
+        });
+
+        //Main navigation Active Class Add Remove
+        $(".navbar-toggler").on("click", function () {
+            $("header").toggleClass("active");
+        });
+        $(document).on("ready", function () {
+            if ($(window).width() > 991) {
+                $("header").removeClass("active");
+            }
+            $(window).on("resize", function () {
+                if ($(window).width() > 991) {
+                    $("header").removeClass("active");
+                }
+            });
+        });
+    </script>
+    <!-- //MENU-JS -->
+
+    <!-- disable body scroll which navbar is in active -->
+    <script>
+        $(function () {
+            $('.navbar-toggler').click(function () {
+                $('body').toggleClass('noscroll');
+            })
+        });
+    </script>
+    <!-- //disable body scroll which navbar is in active -->
+
+    <!--bootstrap-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- //bootstrap-->
+    <!-- //Js scripts -->
+</body>
+
+</html>
